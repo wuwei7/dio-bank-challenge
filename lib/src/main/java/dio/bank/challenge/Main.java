d47a7f507b9979238ada9eb8281669320c56e2ae
Brandon Campos <16386564+wuwei7@users.noreply.github.com>
2022-10-21 18:32:03 -0300

    Change question and add argument to askWhichTransaction()

--- lib/src/main/java/dio/bank/challenge/Main.java
+++ lib/src/main/java/dio/bank/challenge/Main.java
@@ -5,21 +5,29 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("Type 1 for checking account and 2 for savings account: ");
+		System.out.print("Type 1 to create a checking account and 2 to create a savings account: ");
+		scanner();
+		System.out.println();
+		
+	}
+	
+	public static void scanner() {
+		Scanner scanner = new Scanner(System.in);
 		int value = Integer.valueOf(scanner.nextLine());
 		askWhichAccount(value);
-		
 	}
 	
 	public static void askWhichAccount(int value) {
 		if (value == 1) {
 			Account checkingAccount = new CheckingAccount();
-			checkingAccount.askClientWhichTransaction();
-		} else {
+			checkingAccount.askWhichTransaction(0);
+		} else if (value == 2) {
 			Account savingsAccount = new SavingsAccount();
-			savingsAccount.askClientWhichTransaction();
+			savingsAccount.askWhichTransaction(1);
+		} else {
+			System.out.print("Invalid number. Please choose 1 for checking account and 2 for savings account: ");
+			scanner();
 		}
 	}
 
