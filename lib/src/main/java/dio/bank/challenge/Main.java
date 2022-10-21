cfd4ca22c9866806474cf50dd75f9638dc3899e1
Brandon Campos <16386564+wuwei7@users.noreply.github.com>
2022-10-21 08:29:35 -0300

    Add main method

---
+++ lib/src/main/java/dio/bank/challenge/Main.java
@@ -0,0 +1,14 @@
+package dio.bank.challenge;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		Account checkingAccount = new CheckingAccount();
+		
+	}
+
+}
