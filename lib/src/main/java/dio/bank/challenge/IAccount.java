a77838861f6980143c9fc9bd04dd4f37f03a5942
Brandon Campos <16386564+wuwei7@users.noreply.github.com>
2022-10-22 10:29:50 -0300

    Change return type of method withdraw()

--- lib/src/main/java/dio/bank/challenge/IAccount.java
+++ lib/src/main/java/dio/bank/challenge/IAccount.java
@@ -4,7 +4,7 @@ public interface IAccount {
 	
 	void deposit(double amount);
 	
-	void withdraw(double amount);
+	int withdraw(double amount);
 	
 	void transfer(double amount, Account targetAccount);
 	
